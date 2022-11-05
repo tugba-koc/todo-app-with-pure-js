e755a098f6b8af74ec8d10f0955e00537ff02725
tugba-koc <koctug@itu.edu.tr>
2022-11-05 18:57:05 +0300

    feat : add open modal and remove values function

--- js/addTodoController.js
+++ js/addTodoController.js
@@ -2,8 +2,10 @@ const addtodo = () => {
   let modal = document.getElementById('add-todo__modal');
   let btn = document.getElementById('add-todo__btn');
   let cancelButton = document.querySelector('.add-todo__model-cancel');
-  let form = document.querySelector('.add-todo__model-form')
+  let form = document.querySelector('.add-todo__model-form');
   let saveButton = document.querySelector('.add-todo__model-save');
+  let titleInput = document.querySelector('.add-todo__model-input');
+  let dateInput = document.querySelector('.add-todo__model-input.date');
 
   // Create state value for todos
   let todos = [];
@@ -13,25 +15,40 @@ const addtodo = () => {
     modal.style.display = 'block';
   });
 
+  const removeInputValues = () => {
+    titleInput.value = '';
+    dateInput.value = '';
+  };
+
   // When the user clicks on cancel, close the modal
   cancelButton.addEventListener('click', function (e) {
     e.preventDefault();
     modal.style.display = 'none';
+    removeInputValues();
   });
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = 'none';
+      removeInputValues();
     }
   };
 
   // --------------------------------------------------
   // Submit Handler
-  form.addEventListener('submit', function(e){
+  form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log(e);
-  })
+    if (titleInput.value.length && dateInput.value.length) {
+      titleInputVal = titleInput.value.trim();
+      dateInputVal = dateInput.value.trim();
+      todos = [
+        ...todos,
+        { title: titleInputVal, date: dateInputVal, id: Date.now() },
+      ];
+      removeInputValues();
+    }
+  });
 };
 
 export default addtodo;
