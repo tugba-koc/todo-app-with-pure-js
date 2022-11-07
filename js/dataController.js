a16210b6b57174c9f804e490cc9ab4105bcc9e31
tugba-uppercase <tugba@uppercasebrands.com>
2022-11-07 22:38:47 +0300

    feat : localstorage for multiple users

--- js/dataController.js
+++ js/dataController.js
@@ -1,22 +1,40 @@
 const getData = () => {
-  let userName = localStorage.getItem('user');
+  let currentUser = localStorage.getItem('active-user');
   let res = JSON.parse(localStorage.getItem('user-data'));
   if (res) {
-    if (res.user == userName && res.data) {
-      document.querySelector('.add-todo__task-list').innerHTML = res.data;
+    let userDataObj = res.find((el) => el.user == currentUser);
+    if (userDataObj) {
+      if (userDataObj.data) {
+        document.querySelector('.add-todo__task-list').innerHTML =
+          userDataObj.data;
+      }
     }
   }
 };
 
 const saveData = () => {
+  let userDataList = [];
+  let isValid = false;
   let docText = document.querySelector('.add-todo__task-list').innerHTML;
-  localStorage.setItem(
-    'user-data',
-    JSON.stringify({ user: localStorage.getItem('user'), data: docText })
-  );
+  userDataList = JSON.parse(localStorage.getItem('user-data'));
+  if (!userDataList) {
+    userDataList = [
+      { user: localStorage.getItem('active-user'), data: docText },
+    ];
+  } else {
+    isValid = userDataList.find(
+      (el) => el.user == localStorage.getItem('active-user')
+    );
+    console.log(isValid);
+    userDataList.push({ user: localStorage.getItem('active-user'), data: '' });
+    userDataList = userDataList.map((el) =>
+      el.user == localStorage.getItem('active-user')
+        ? { user: localStorage.getItem('active-user'), data: docText }
+        : el
+    );
+  }
+
+  localStorage.setItem('user-data', JSON.stringify(userDataList));
 };
 
-export {
-    getData,
-    saveData
-}
+export { getData, saveData };
--- js/signInUpController.js
+++ js/signInUpController.js
@@ -5,9 +5,19 @@ const signInUp = () => {
   let inputValue = '';
 
   signInBtn.addEventListener('click', () => {
+    let prevUsers = [];
     inputValue = signInInput.value;
     if (inputValue.trim().length) {
-      localStorage.setItem('user', inputValue);
+      prevUsers = JSON.parse(localStorage.getItem('user'));
+      if (!prevUsers) {
+        prevUsers = [inputValue];
+      } else {
+        if(!prevUsers.includes(inputValue)){
+          prevUsers.push(inputValue);
+        }
+      }
+      localStorage.setItem('user', JSON.stringify(prevUsers));
+      localStorage.setItem('active-user', inputValue);
       signInDirection.href = '/addToDo';
     } else {
       alert('Please type a valid username');
